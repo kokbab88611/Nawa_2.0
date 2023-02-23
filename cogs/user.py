6666a81c63df827988dafafb315827618e4929ba
오곡밥튀김 <38698459+kokbab88611@users.noreply.github.com>
2023-02-24 04:48:29 +0900

    USER 오류 해결 시도

--- cogs/user.py
+++ cogs/user.py
@@ -6,7 +6,7 @@ import json
 import os
 import asyncio
 import random
-
+import functools
 
 #client = pymongo.MongoClient("mongodb+srv://hyun88611:hyun@88611@cluster0.d0nl1ss.mongodb.net/?retryWrites=true&w=majority")
 #db = client.userInformation
@@ -40,46 +40,47 @@ class User(commands.Cog):
   async def on_ready(self):
     print("준비됨")
   
-  async def saveUser(func):
-    async def wrapper(self, ctx):
-      await func()
-      with open(os.path.join(__location__ , r'\json\users.json')) as f:
-        data = json.load(f)
-        keys = [users for users in data]
-        if ctx.author.id not in data:
-          data[ctx.author.id] = {
-          "level" : {
-            "main":1,
-            "xp": 0,
-            "rangi":0,
-            "cheeyi":0,
-            "saehee":0
-          },
-          "money" : 0,
-          "item": [],
-          "attendence": False
-          }
-          json.dump(data, f)
-        else:
-          random_xp = random.randint(1,2) 
-          self.userData[ctx.author.id]["level"]["xp"] += random_xp
-          
-          if self.level_up(ctx.author.id):
-            await ctx.send("레벨업{}".format(self.userData[ctx.author.id]["level"]["xp"]))
+  def saveUser(self):
+    def wrapper(func):
+      @functools.wraps(func)
+      async def wrapped(ctx):
+        with open(os.path.join(__location__ , r'\json\users.json')) as f:
+          data = json.load(f)
+          keys = [users for users in data]
+          if ctx.author.id not in data:
+            data[ctx.author.id] = {
+            "level" : {
+              "main":1,
+              "xp": 0,
+              "rangi":0,
+              "cheeyi":0,
+              "saehee":0
+            },
+            "money" : 0,
+            "item": [],
+            "attendence": False
+            }
+            json.dump(data, f)
+          else:
+            random_xp = random.randint(1,2) 
+            self.userData[ctx.author.id]["level"]["xp"] += random_xp
             
-      return wrapper
+            if self.level_up(ctx.author.id):
+              ctx.send("level up {}".format(self.userData[ctx.author.id]["level"]["xp"]))
+        return await func()    
+      return wrapped
+    return wrapper
 
-"""
   @commands.command(name="핑")
-  @saveUser
+  @saveUser()
   async def ping(self, ctx):
-    await ctx.send("퐁이니라!")
+    await ctx.send("pong!!")
   
 
   @commands.hybrid_command(with_app_command=True)
   @saveUser
   async def 아(self, ctx):
       await ctx.send("This is a hybrid command!")
-"""
+
 async def setup(bot):
   await bot.add_cog(User(bot))
