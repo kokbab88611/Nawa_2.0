7442eaecfbf4f0ed2d68db24a4cd885324b2f038
yanbunnyandere <69045157+yanbunnyandere@users.noreply.github.com>
2023-02-25 20:38:05 +0900

    Update game.py

--- cogs/game.py
+++ cogs/game.py
@@ -5,42 +5,54 @@ from discord.ui import Button, View
 from discord.ext import commands, tasks
 
 class RcpButtons(Button):
-    def __init__(self, label, emoji, custom_id, command_userid):
+    def __init__(self, label, emoji, custom_id, command_userid, bet_money):
         super().__init__(label=label, style=discord.ButtonStyle.green, emoji=emoji, custom_id=custom_id)
-        self.custom_id, self.user_rcp, self.command_userid = str(custom_id), emoji + label, command_userid
+        self.custom_id, self.user_rcp, self.command_userid, self.bet_money = str(custom_id), emoji + label, command_userid, bet_money
 
     async def rcp_result(user_rcp):
-            rcp_num = random.randint(1,3)
-            if rcp_num == 1:
-                bot_rcp = "✌️가위"
-                if user_rcp == "scissors":
-                    result = "비김"
-                elif user_rcp == "rock":
-                    result = "이김"
-                else:
-                    result = "짐"
-            elif rcp_num == 2:
-                bot_rcp = "✊바위"
-                if user_rcp == "scissors":
-                    result = "짐"
-                elif user_rcp == "rock":
-                    result = "비김"
-                else:
-                    result = "이김"
+        rcp_num = random.randint(1,3)
+        if rcp_num == 1:
+            bot_rcp = "✌️가위"
+            if user_rcp == "scissors":
+                result = "draw"
+            elif user_rcp == "rock":
+                result = "win"
             else:
-                bot_rcp = "✋보"
-                if user_rcp == "scissors":
-                    result = "이김"
-                elif user_rcp == "rock":
-                    result = "짐"
-                else:
-                    result = "비김"
-            return bot_rcp, result
+                result = "lose"
+        elif rcp_num == 2:
+            bot_rcp = "✊바위"
+            if user_rcp == "scissors":
+                result = "lose"
+            elif user_rcp == "rock":
+                result = "draw"
+            else:
+                result = "win"
+        else:
+            bot_rcp = "✋보"
+            if user_rcp == "scissors":
+                result = "win"
+            elif user_rcp == "rock":
+                result = "lose"
+            else:
+                result = "draw"
+        return bot_rcp, result
 
     async def callback(self, interaction):
         if interaction.user.id == self.command_userid:
             bot_rcp, result = await RcpButtons.rcp_result(self.custom_id)
-            embed = discord.Embed(title=result, description=f'페이:{bot_rcp}\n나:{self.user_rcp}', color=0xb0a7d3)
+            if result == "win":
+                result = "이김"
+                message = f'{self.bet_money} 얻음'
+                #money_increase(self.bet_money, user)
+            elif result == "draw":
+                result = "비김"
+                message = f'{int(round(self.bet_money * 0.5, 0))} 잃음'
+                #money_decrease(int(round(self.bet_money * 0.5, 0)), user)
+            else:
+                result = "짐"
+                message = f'{self.bet_money} 잃음'
+                #money_decrease(self.bet_money, user)
+            embed = discord.Embed(title=result, description=f'페이:{bot_rcp}\n나:{self.user_rcp}\n{message}', color=0xb0a7d3)
             await interaction.response.edit_message(content="", embed=embed, view=None)
         else:
             await interaction.response.send_message(content="너 이거 못눌러", ephemeral=True)
@@ -55,13 +67,17 @@ class Game(commands.Cog):
         print("준비됨")
 
     @app_commands.command(name="가위바위보", description="폐이와 가위바위보를 합니다")
-    async def buttontest(self, interaction: discord.Interaction):
-        view = View()
-        view.add_item(RcpButtons('가위', "✌️", "scissors", interaction.user.id))
-        view.add_item(RcpButtons('바위', "✊", "rock", interaction.user.id))
-        view.add_item(RcpButtons('보', "✋", "paper", interaction.user.id))
-        embed = discord.Embed(title='가위바위보중에 하나 골라')
-        await interaction.response.send_message(embed=embed, view=view)
+    async def buttontest(self, interaction: discord.Interaction, bet_money: int):
+        #if bet_money > owned_money:
+        if bet_money:
+            view = View()
+            view.add_item(RcpButtons('가위', "✌️", "scissors", interaction.user.id, bet_money))
+            view.add_item(RcpButtons('바위', "✊", "rock", interaction.user.id, bet_money))
+            view.add_item(RcpButtons('보', "✋", "paper", interaction.user.id, bet_money))
+            embed = discord.Embed(title='가위바위보중에 하나 골라')
+            await interaction.response.send_message(embed=embed, view=view)
+        else:
+            await interaction.response.send_message(content="돈 부족. 너 돈 필요.", ephemeral=True)
 
     @app_commands.command(name="추첨", description="추첨기를 생성합니다")
     async def raname(self, interaction: discord.Interaction, people: str, join: int = 1):
