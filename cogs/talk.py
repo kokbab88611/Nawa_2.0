665013abe21d46a9c6773c7351838cc6867653d3
오곡밥튀김 <38698459+kokbab88611@users.noreply.github.com>
2023-02-28 03:24:15 +0900

    talk 수정

--- cogs/talk.py
+++ cogs/talk.py
@@ -7,7 +7,7 @@ import os
 import random
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-all_hi = ["안녕", "어서와", "히사시부리", "하이", "반가워", "오랜만이야", "나 또 왔어", "올만", "또 보네", 
+all_hi = ["안녀", "안녕", "안뇽", "안뇨", "어서와", "히사시부리", "하이", "반가워", "오랜만이야", "나 또 왔어", "올만", "또 보네", 
         "좋은 아침", "잘 잤어", "좋은 밤", "좋은 저녁", "좋은 점심", "여기야", "반갑다", 
         "돌아왔", "나 왔어", "나 왔", "갔다 왔어", "다녀왔"]
 
@@ -28,9 +28,9 @@ class Talk(commands.Cog):
         print("인사", any(x in all_hi for x in message.content))
         print("랑이", ("랑이야" in contents))
 
-        if any(x in all_hi for x in message.content):
-            print(message.content)
-            await message.channel.send("안녕하느냐!")
+        if any(x in message.content for x in all_hi) and "랑이야" in message.content:
+                print(message.content)
+                await message.channel.send("안녕하느냐!")
 
 async def setup(bot):
     await bot.add_cog(Talk(bot))
