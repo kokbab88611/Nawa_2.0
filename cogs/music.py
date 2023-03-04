7c4b5e1f135f892952656fd49720990e66a2b07b
yanbunnyandere <69045157+yanbunnyandere@users.noreply.github.com>
2023-03-04 19:37:48 +0900

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -1,4 +1,5 @@
 import discord, nacl
+import asyncio
 from discord import app_commands
 from discord.ext import commands
 import yt_dlp
@@ -6,40 +7,40 @@ import os
 import random
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-#FFMPEG_PATH = os.path.join(f"{__location__}\\ffmpeg\\bin\\ffmpeg.exe")
-
 class Music(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
 
     @app_commands.command(name="재생", description="음원을 재생합니다")
     async def play(self, interaction: discord.Interaction, url: str):
-        vc = self.bot.voice_clients[0]
-        YDL_OPTIONS = {'format': 'bestaudio'}
-        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+        yt_dl_opts = {'format': 'bestaudio/best'}
+        ytdl = yt_dlp.YoutubeDL(yt_dl_opts)
+        ffmpeg_options = {'options': "-vn"}
 
-        #if not vc.is_playing() and not vc.is_paused():
-        with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
-            info = ydl.extract_info(url, download=False)
-            URL = info['formats'][0]['url']
-            #source = await discord.FFmpegPCMAudio(URL, **FFMPEG_OPTIONS)
-            #source = await discord.FFmpegOpusAudio.from_probe(URL, method='fallback')
-            source = await discord.FFmpegOpusAudio.from_probe(URL, **FFMPEG_OPTIONS)
-            vc.play(source)
+        voice_client = await interaction.user.voice.channel.connect()
+        self.bot.voice_clients[0] = voice_client
 
+        loop = asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
 
-    @app_commands.command(name="입장", description="통화방에 연결합니다")
-    async def join(self, interaction: discord.Interaction):
-        channel = interaction.user.voice.channel
-        await channel.connect()
+        song = data['url']
+        player = discord.FFmpegPCMAudio(song, **ffmpeg_options)
 
-    
+        voice_client.play(player)
+
+    @app_commands.command(name="일시정지", description="재생을 일시정지합니다")
+    async def pause(self, interaction: discord.Interaction):
+        self.bot.voice_clients[0].pause()
+
+    @app_commands.command(name="재개", description="재생을 재개합니다")
+    async def resume(self, interaction: discord.Interaction):
+        self.bot.voice_clients[0].resume()
 
     @app_commands.command(name="퇴장", description="통화방에서 나갑니다")
     async def leave(self, interaction: discord.Interaction):
         await self.bot.voice_clients[0].disconnect()
     
-
+    
 async def setup(bot):
     await bot.add_cog(Music(bot))
     
