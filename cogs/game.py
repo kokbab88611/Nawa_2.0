9e39f2ef525968f958602fca7eff09e41d6fe225
yanbunnyandere <69045157+yanbunnyandere@users.noreply.github.com>
2023-06-25 00:02:45 +0900

    대충 카드뒤집기 만들기 시작함

--- cogs/game.py
+++ cogs/game.py
@@ -3,6 +3,56 @@ import asyncio
 from discord import app_commands,Interaction,Reaction,InteractionResponse
 from discord.ui import Button, View
 from discord.ext import commands, tasks
+import random
+
+class MemoryGameDropDown(discord.ui.Select):
+    def __init__(self):
+        options = [
+            discord.SelectOption(
+                label="A1", value="A1"),
+            discord.SelectOption(
+                label="A2", value="A2"),
+            discord.SelectOption(
+                label="A3", value="A3"),
+            discord.SelectOption(
+                label="A4", value="A4"),
+            discord.SelectOption(
+                label="B1", value="B1"),
+            discord.SelectOption(
+                label="B2", value="B2"),
+            discord.SelectOption(
+                label="B3", value="B3"),
+            discord.SelectOption(
+                label="B4", value="B4"),
+            discord.SelectOption(
+                label="C1", value="C1"),
+            discord.SelectOption(
+                label="C2", value="C2"),
+            discord.SelectOption(
+                label="C3", value="C3"),
+            discord.SelectOption(
+                label="C4", value="C4"),
+            discord.SelectOption(
+                label="D1", value="D1"),
+            discord.SelectOption(
+                label="D2", value="D2"),
+            discord.SelectOption(
+                label="D3", value="D3"),
+            discord.SelectOption(
+                label="D4", value="D4"),
+        ]
+
+        super().__init__(placeholder="원하시는 카드를 선택하십시오", options=options, min_values=2, max_values=2)
+
+    async def callback(self, interaction: discord.Interaction):
+        
+        view = MemoryGameView()
+        await interaction.response.edit_message(content=f"{self.values[0]} 과 {self.values[1]} 을 선택하셨습니다", view=view)
+
+class MemoryGameView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.add_item(MemoryGameDropDown())
 
 class Game(commands.Cog):
     channel_id:string
@@ -31,7 +81,19 @@ class Game(commands.Cog):
             embed.add_field(name="당첨자", value=arry, inline=True)
             await interaction.response.send_message(embed=embed)
 
-
+    @app_commands.command(name="카드짝", description="카드 짝 맞추기 게임을 플레이합니다")
+    async def MemoryGame(self, interaction: discord.Interaction):
+        cards = random.shuffle(["1️⃣", "1️⃣", "2️⃣", "2️⃣", "3️⃣", "3️⃣", "4️⃣", "4️⃣", "5️⃣", "5️⃣", "6️⃣", "6️⃣", "7️⃣", "7️⃣", "8️⃣", "8️⃣"])
+        base= """⬛1️⃣2️⃣3️⃣4️⃣
+        🇦⬜⬜⬜⬜
+        🇧⬜⬜⬜⬜
+        🇨⬜⬜⬜⬜
+        🇩⬜⬜⬜⬜"""
+        embed = discord.Embed(
+                title="카드 짝 맞추기",
+                description=base)
+        view = MemoryGameView()
+        await interaction.response.send_message(embed=embed, view=view)
 
 async def setup(bot):
     await bot.add_cog(Game(bot))
